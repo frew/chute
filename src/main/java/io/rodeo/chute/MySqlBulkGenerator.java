82acbc07ffcb54fda7bb6163d6c168879d1476fb
Fred Wulff <frew@cs.stanford.edu>
2015-12-29 18:03:58 -0600

    Add bulk loader for testing

---
+++ src/main/java/io/rodeo/chute/MySqlBulkGenerator.java
@@ -0,0 +1,23 @@
+package io.rodeo.chute;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Random;
+
+public class MySqlBulkGenerator {
+	public static void main(String[] args) throws InstantiationException, IllegalAccessException, ClassNotFoundException, SQLException {
+		// TODO Auto-generated method stub
+		Class.forName("com.mysql.jdbc.Driver").newInstance();
+		Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/chute_test", "root", "test");
+		PreparedStatement insertStmt = conn.prepareStatement("INSERT INTO testb(id, comment, user_id) VALUES(?, ?, ?)");
+		Random r = new Random();
+		for (int i = 0; i < 1000000; i++) {
+			insertStmt.setInt(1, r.nextInt());
+			insertStmt.setString(2, "Hello " + i);
+			insertStmt.setInt(3, r.nextInt());
+			insertStmt.execute();
+		}
+	}
+}
