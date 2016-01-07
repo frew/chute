3a9e0614264cbf991fdf817930ac12f19f148d90
Fred Wulff <frew@cs.stanford.edu>
2016-01-07 14:51:31 -0600

    Use row wrapper in importer

--- src/main/java/io/rodeo/chute/mysql/MySqlIterativeImporter.java
+++ src/main/java/io/rodeo/chute/mysql/MySqlIterativeImporter.java
@@ -1,7 +1,7 @@
 package io.rodeo.chute.mysql;
 
-import java.io.EOFException;
-import java.io.File;
+import io.rodeo.chute.Row;
+
 import java.io.IOException;
 import java.io.Serializable;
 import java.util.BitSet;
@@ -11,7 +11,6 @@ import java.util.Map.Entry;
 
 import com.github.shyiko.mysql.binlog.BinaryLogClient;
 import com.github.shyiko.mysql.binlog.BinaryLogClient.EventListener;
-import com.github.shyiko.mysql.binlog.BinaryLogFileReader;
 import com.github.shyiko.mysql.binlog.event.DeleteRowsEventData;
 import com.github.shyiko.mysql.binlog.event.Event;
 import com.github.shyiko.mysql.binlog.event.TableMapEventData;
@@ -23,7 +22,7 @@ public class MySqlIterativeImporter implements EventListener {
 	private final int port;
 	private final String user;
 	private final String password;
-	
+
 	private BinaryLogClient client;
 	private MySqlBinaryLogPosition position;
 
@@ -35,7 +34,7 @@ public class MySqlIterativeImporter implements EventListener {
 		this.password = password;
 		this.position = position;
 	}
-	
+
 	public void run() throws IOException {
 		client = new BinaryLogClient(host, port, user, password);
 		client.setBinlogFilename(position.getFilename());
@@ -43,49 +42,68 @@ public class MySqlIterativeImporter implements EventListener {
 		client.registerEventListener(this);
 		client.connect();
 	}
-	
+
 	private Map<Long, TableMapEventData> tableMap = new HashMap<Long, TableMapEventData>();
-	
-	private void processRowChange(Long tableId, BitSet includedColsBeforeUpdate, BitSet includedCols, Serializable[] oldRow, Serializable[] newRow) {
+
+	private void processRowChange(Long tableId, BitSet includedColsBeforeUpdate, BitSet includedCols, Row oldRow, Row newRow) {
 		TableMapEventData tmEvent = tableMap.get(tableId);
 		if (tmEvent == null) {
+			// TODO: Where do exceptions in the event loop go?
 			throw new IllegalStateException("No table map entry for " + tableId);
 		}
 		String database = tmEvent.getDatabase();
 		String table = tmEvent.getTable();
+
+		// Enforce that all of the row is there or not.
+		// This is default in MySQL.
+		// TODO: Add support for other modes.
+		if (includedColsBeforeUpdate != null
+				&& includedColsBeforeUpdate.cardinality() != includedColsBeforeUpdate.length()) {
+			throw new IllegalStateException("Expected all columns to be included");
+		}
+		if (includedCols != null
+				&& includedCols.cardinality() != includedCols.length()) {
+			throw new IllegalStateException("Expected all columns to be included");
+		}
+
+
+		System.out.println("RC " + database + "." + table + " -> " + oldRow + " : " + newRow);
 	}
-	
+
 	@Override
 	public void onEvent(Event event) {
 		switch (event.getHeader().getEventType()) {
 		case EXT_WRITE_ROWS:
 			WriteRowsEventData wrEvent = (WriteRowsEventData) event.getData();
 			for (Serializable[] row: wrEvent.getRows()) {
-				processRowChange(wrEvent.getTableId(), null, wrEvent.getIncludedColumns(), null, row);
+				processRowChange(wrEvent.getTableId(), null, wrEvent.getIncludedColumns(), null, new Row(row));
 			}
 			break;
 		case EXT_UPDATE_ROWS:
 			UpdateRowsEventData urEvent = (UpdateRowsEventData) event.getData();
 			for (Entry<Serializable[], Serializable[]> rowChange: urEvent.getRows()) {
-				processRowChange(urEvent.getTableId(), urEvent.getIncludedColumnsBeforeUpdate(), urEvent.getIncludedColumns(), rowChange.getKey(), rowChange.getValue());
+				processRowChange(urEvent.getTableId(), urEvent.getIncludedColumnsBeforeUpdate(), urEvent.getIncludedColumns(), new Row(rowChange.getKey()), new Row(rowChange.getValue()));
 			}
 			break;
 		case EXT_DELETE_ROWS:
 			DeleteRowsEventData drEvent = (DeleteRowsEventData) event.getData();
 			for (Serializable[] row: drEvent.getRows()) {
-				processRowChange(drEvent.getTableId(), drEvent.getIncludedColumns(), null, row, null);
+				processRowChange(drEvent.getTableId(), drEvent.getIncludedColumns(), null, new Row(row), null);
 			}
 			break;
 		case TABLE_MAP:
 			TableMapEventData tmData = (TableMapEventData) event.getData();
-			System.out.println("Adding table map data for " + tmData.getTableId() + " -> " + tmData.getTable());
+			if (!tableMap.containsKey(tmData.getTableId())) {
+				System.out.println("Adding table map data for " + tmData.getTableId() + " -> " + tmData.getTable());
+			}
 			tableMap.put(tmData.getTableId(), tmData);
 			break;
 		default:
-			System.out.println("Not handling " + event.getHeader().getEventType());
+			// System.out.println("Not handling " + event);
+			break;
 		}
 	}
-	
+
 	public static void main(String[] args) throws IOException {
 		MySqlBinaryLogPosition pos = new MySqlBinaryLogPosition();
 		MySqlIterativeImporter importer = new MySqlIterativeImporter(
