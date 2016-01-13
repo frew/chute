aa9f99da8a364f6c5eeba5ad15f6c26487de2282
Fred Wulff <frew@cs.stanford.edu>
2016-01-13 13:15:22 -0800

    Remove debugging exception

--- src/main/java/io/rodeo/chute/PrintingStreamProcessor.java
+++ src/main/java/io/rodeo/chute/PrintingStreamProcessor.java
@@ -21,7 +21,6 @@ public class PrintingStreamProcessor implements StreamProcessor {
 	@Override
 	public void process(TableSchema schema, Row oldRow, Row newRow,
 			StreamPosition pos) {
-		if (true) throw new RuntimeException("blash");
 		System.out.println("Got " + schema + ": " + oldRow + " -> " + newRow + " @ " + pos);
 	}
 
