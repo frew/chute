02cb0bc99e738edb8a38deb1fbf979b70f505f34
Fred Wulff <frew@cs.stanford.edu>
2016-01-13 17:03:50 -0800

    Break out MySqlFullTableImportManager into its own class

---
+++ src/main/java/io/rodeo/chute/mysql/MySqlFullTableImportManager.java
@@ -0,0 +1,109 @@
+package io.rodeo.chute.mysql;
+
+import io.rodeo.chute.Key;
+import io.rodeo.chute.Split;
+import io.rodeo.chute.StreamProcessor;
+import io.rodeo.chute.mysql.MySqlImportManager.SplitFullImportState;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.concurrent.Semaphore;
+
+class MySqlFullTableImportManager implements Runnable {
+	public final MySqlTableSchema schema;
+	public final Map<Split, SplitFullImportState> fullImportStates;
+	public final MySqlFullImporter importer;
+	public final StreamProcessor processor;
+	private ConnectionManager connManager;
+	private final Semaphore concurrentImportSemaphore;
+	private final int epoch;
+
+	public MySqlFullTableImportManager(MySqlTableSchema schema,
+			StreamProcessor processor, ConnectionManager connManager,
+			Semaphore concurrentImportSemaphore, int epoch, int batchSize) {
+		this.schema = schema;
+		this.fullImportStates = new HashMap<Split, SplitFullImportState>();
+		this.importer = new MySqlFullImporter(schema, batchSize);
+		this.processor = processor;
+		this.connManager = connManager;
+		this.concurrentImportSemaphore = concurrentImportSemaphore;
+		this.epoch = epoch;
+	}
+
+	private void initializeSplits(Connection conn) throws SQLException {
+		System.out.println("Initializing splits");
+		Iterator<Key> splitIt = importer.createSplitPointIterator(conn);
+		Key previousSplitPoint = null;
+		while (splitIt.hasNext()) {
+			Key splitPoint = splitIt.next();
+			fullImportStates.put(new Split(previousSplitPoint, splitPoint),
+					SplitFullImportState.NOT_STARTED);
+		}
+		fullImportStates.put(new Split(previousSplitPoint, null),
+				SplitFullImportState.NOT_STARTED);
+		System.out.println("Initialized " + fullImportStates.size()
+				+ " splits");
+	}
+
+	private Split getSplitToRun() {
+		for (Entry<Split, SplitFullImportState> splitEnt : fullImportStates
+				.entrySet()) {
+			if (splitEnt.getValue() == SplitFullImportState.NOT_STARTED) {
+				return splitEnt.getKey();
+			}
+		}
+		return null;
+	}
+
+	private void setFullImportRunning(Split split) {
+		concurrentImportSemaphore.acquireUninterruptibly();
+		fullImportStates.put(split, SplitFullImportState.RUNNING);
+	}
+
+	private void setFullImportDone(Split split) {
+		fullImportStates.put(split, SplitFullImportState.DONE);
+		concurrentImportSemaphore.release();
+	}
+
+	private void startFullImport(final Split split, final Connection conn,
+			final int epoch, StreamProcessor processor) throws SQLException {
+		setFullImportRunning(split);
+		Runnable doneCb = new Runnable() {
+			@Override
+			public void run() {
+				try {
+					connManager.returnConnection(conn);
+				} catch (SQLException e) {
+					// TODO: More intelligent error handling
+					e.printStackTrace();
+				}
+				setFullImportDone(split);
+			}
+		};
+		Runnable r = importer.createImporterRunnable(conn, epoch, split,
+				processor, doneCb);
+		new Thread(r).start();
+	}
+
+	@Override
+	public void run() {
+		try {
+			Connection splitConn = connManager.createConnection();
+			initializeSplits(splitConn);
+			connManager.returnConnection(splitConn);
+			Split split;
+
+			while ((split = getSplitToRun()) != null) {
+				Connection conn = connManager.createConnection();
+				startFullImport(split, conn, epoch, processor);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+			System.exit(1);
+		}
+	}
+}
--- src/main/java/io/rodeo/chute/mysql/MySqlImportManager.java
+++ src/main/java/io/rodeo/chute/mysql/MySqlImportManager.java
@@ -16,18 +16,12 @@ package io.rodeo.chute.mysql;
  limitations under the License.
  */
 
-import io.rodeo.chute.Key;
-import io.rodeo.chute.Split;
 import io.rodeo.chute.StreamProcessor;
 
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Iterator;
 import java.util.List;
-import java.util.Map;
-import java.util.Map.Entry;
 import java.util.concurrent.Semaphore;
 
 public class MySqlImportManager {
@@ -46,97 +40,6 @@ public class MySqlImportManager {
 	private final int epoch;
 	private final List<MySqlFullTableImportManager> fullImportManagers;
 
-	private class MySqlFullTableImportManager implements Runnable {
-		public final MySqlTableSchema schema;
-		public final Map<Split, SplitFullImportState> fullImportStates;
-		public MySqlFullImporter importer;
-		public StreamProcessor processor;
-		private ConnectionManager connManager;
-
-		public MySqlFullTableImportManager(MySqlTableSchema schema,
-				StreamProcessor processor, ConnectionManager connManager) {
-			this.schema = schema;
-			this.fullImportStates = new HashMap<Split, SplitFullImportState>();
-			this.importer = new MySqlFullImporter(schema, BATCH_SIZE);
-			this.processor = processor;
-			this.connManager = connManager;
-		}
-
-		private void initializeSplits(Connection conn) throws SQLException {
-			System.out.println("Initializing splits");
-			Iterator<Key> splitIt = importer.createSplitPointIterator(conn);
-			Key previousSplitPoint = null;
-			while (splitIt.hasNext()) {
-				Key splitPoint = splitIt.next();
-				fullImportStates.put(new Split(previousSplitPoint, splitPoint),
-						SplitFullImportState.NOT_STARTED);
-			}
-			fullImportStates.put(new Split(previousSplitPoint, null),
-					SplitFullImportState.NOT_STARTED);
-			System.out.println("Initialized " + fullImportStates.size()
-					+ " splits");
-		}
-
-		private Split getSplitToRun() {
-			for (Entry<Split, SplitFullImportState> splitEnt : fullImportStates
-					.entrySet()) {
-				if (splitEnt.getValue() == SplitFullImportState.NOT_STARTED) {
-					return splitEnt.getKey();
-				}
-			}
-			return null;
-		}
-
-		private void setFullImportRunning(Split split) {
-			activeFullImports.acquireUninterruptibly();
-			;
-			fullImportStates.put(split, SplitFullImportState.RUNNING);
-		}
-
-		private void setFullImportDone(Split split) {
-			fullImportStates.put(split, SplitFullImportState.DONE);
-			activeFullImports.release();
-		}
-
-		private void startFullImport(final Split split, final Connection conn,
-				final int epoch, StreamProcessor processor) throws SQLException {
-			setFullImportRunning(split);
-			Runnable doneCb = new Runnable() {
-				@Override
-				public void run() {
-					try {
-						connManager.returnConnection(conn);
-					} catch (SQLException e) {
-						// TODO: More intelligent error handling
-						e.printStackTrace();
-					}
-					setFullImportDone(split);
-				}
-			};
-			Runnable r = importer.createImporterRunnable(conn, epoch, split,
-					processor, doneCb);
-			new Thread(r).start();
-		}
-
-		@Override
-		public void run() {
-			try {
-				Connection splitConn = connManager.createConnection();
-				initializeSplits(splitConn);
-				connManager.returnConnection(splitConn);
-				Split split;
-
-				while ((split = getSplitToRun()) != null) {
-					Connection conn = connManager.createConnection();
-					startFullImport(split, conn, epoch, processor);
-				}
-			} catch (SQLException e) {
-				e.printStackTrace();
-				System.exit(1);
-			}
-		}
-	}
-
 	public MySqlImportManager(List<MySqlTableSchema> schemas,
 			StreamProcessor processor, int epoch, ConnectionManager connManager) {
 		this.fullImportManagers = new ArrayList<MySqlFullTableImportManager>();
@@ -145,7 +48,7 @@ public class MySqlImportManager {
 		this.connManager = connManager;
 		for (MySqlTableSchema schema : schemas) {
 			this.fullImportManagers.add(new MySqlFullTableImportManager(schema,
-					processor, connManager));
+					processor, connManager, activeFullImports, epoch, BATCH_SIZE));
 		}
 
 	}
